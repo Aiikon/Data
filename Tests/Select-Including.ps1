e5072465ad156c8bed3e9ae6a9b71b98578231a1
Aiikon <47838475+Aiikon@users.noreply.github.com>
2022-08-06 10:37:11 -0700

    Adding edge case test notes with key comparisons
    - When making keys using $(foreach) -join as most functions do; an if ($false) {} statement results in a key value not being added to the join. This creates issues when compared to a key with a $null or empty value instead. If this behavior is to be changed it needs to be done in every function.

--- Tests/Select-Including.ps1
+++ Tests/Select-Including.ps1
@@ -25,5 +25,15 @@ Describe "Select-Including" {
             @($result).Count | Should Be 1
             $result.PropA | Should Be 1
         }
+
+        It "Edge case when comparing null and empty objects" {
+            # TBD if this behavior needs to be reversed; right now if ($false) { }
+            # results in a non-null value that is skipped in the $(foreach ($key) { })
+            # loop. If this is to change it needs to change everywhere.
+            $result = [pscustomobject]@{PropA=1; PropB=if ($false) { 2 }} |
+                Select-Including PropA, PropB ([pscustomobject]@{PropA=1; PropB=$null})
+
+            @($result).Count | Should Be 0
+        }
     }
 }
